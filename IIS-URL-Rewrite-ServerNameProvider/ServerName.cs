2cab77dce2f3daf81d798f7069ea25227654b170
ThePieMonster <zacbraasch@gmail.com>
2020-09-16 23:20:09 -0500

    Initial commit

---
+++ IIS-URL-Rewrite-ServerNameProvider/ServerName.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Web.Iis.Rewrite;
+
+namespace IIS_URL_Rewrite_ServerNameProvider
+{
+    public class ServerName : IRewriteProvider
+    {
+        public void Initialize(IDictionary<string, string> settings, IRewriteContext rewriteContext)
+        {
+        }
+
+        public string Rewrite(string value)
+        {
+            return System.Environment.MachineName;
+        }
+    }
+}
